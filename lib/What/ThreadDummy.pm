18d68225b601ff727646a8f828a124ab83342973
Bryan Matsuo <bryan.matsuo@gmail.com>
2010-11-21 05:55:14 -0800

    Fixed warning when threads module is present.

---
+++ lib/What/ThreadDummy.pm
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+# Use perldoc or option --man to read documentation
+package threads;
+    sub running { }
+
+    sub joinable { }
+
+package What::ThreadDummy;
+
+use strict;
+use warnings;
+use Carp;
+
+our $VERSION = "0.0_1";
+
+return 1;
+
+__END__
+
+=head1 NAME
+
+ThreadDummy - PUT A SHORT DESCRIPTION HERE!
+
+=head1 VERSION
+
+Version 0.0_1
+
+Originally created on 11/21/10 05:34:13
+
+=head1 DESCRIPTION
+
+
+
+=head1 AUTHOR
+
+Bryan Matsuo [bryan.matsuo@gmail.com]
+
+=head1 BUGS
+
+=over
+
+=back
+
+=head1 COPYRIGHT & LICENCE
+
+(c) Bryan Matsuo [bryan.matsuo@gmail.com]
